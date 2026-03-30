a2d626b5d299da0147d7cdf8b3295652ac079e65
OZKR_64 <oscarrodolfoeo@gmail.com>
2026-03-30 05:24:07 -0300

    Docs

--- inventario.py
+++ inventario.py
@@ -1,1 +1,9 @@
-# Proyecto "Sistema de Control de Inventario"
+# Mensaje de bienvenida
+print("🛒 Sistema de Control de Inventario")
+
+# Solicitando información al usuario
+producto = input("Ingrese el nombre del producto: ")
+cantidad = int(input("Ingrese la cantidad inicial en inventario: "))
+precio_unitario = float(input("Ingrese el precio unitario del producto: "))
+
+# Cálculo del valor total del inventario

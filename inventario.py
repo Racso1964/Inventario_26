54896d51b3ade594f8521f2923afc468e8896144
OZKR_64 <oscarrodolfoeo@gmail.com>
2026-04-01 00:20:45 -0300

    refactor: mejorar salida formateada con f-string

--- inventario.py
+++ inventario.py
@@ -7,3 +7,11 @@ cantidad = int(input("Ingrese la cantidad inicial en inventario: "))
 precio_unitario = float(input("Ingrese el precio unitario del producto: "))
 
 # Cálculo del valor total del inventario
+valor_total = cantidad * precio_unitario
+
+# Mostrando los datos formateados
+print("\n📦 Inventario Inicial")
+print(f"Producto: {producto}")
+print(f"Cantidad: {cantidad}")
+print(f"Precio unitario: $ {precio_unitario:.2f}")
+print(f"Valor total del inventario: $ {valor_total:.2f}")

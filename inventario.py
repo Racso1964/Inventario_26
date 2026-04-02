2b1deed420181a9a0910a87c73736c1b0a52f0ad
OZKR_64 <oscarrodolfoeo@gmail.com>
2026-04-02 05:39:35 -0300

    feat: normaliza el nombre del producto al capturarlo

--- inventario.py
+++ inventario.py
@@ -15,3 +15,9 @@ print(f"Producto: {producto}")
 print(f"Cantidad: {cantidad}")
 print(f"Precio unitario: $ {precio_unitario:.2f}")
 print(f"Valor total del inventario: $ {valor_total:.2f}")
+print("🛒 Sistema de Control de Inventario")
+
+# Solicitando información al usuario
+producto = input("Ingrese el nombre del producto: ").strip().title()  # Elimina espacios extra y ajusta la capitalización
+cantidad = int(input("Ingrese la cantidad inicial en inventario: "))
+precio_unitario = float(input("Ingrese el precio unitario del producto: "))
